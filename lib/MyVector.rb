63289967e8e497123890ee88aee1fe7d388a8c90
Courey Elliott <coureyelliott@gmail.com>
2014-07-22 15:13:26 -0500

    possibly working angle calculation

--- lib/MyVector.rb
+++ lib/MyVector.rb
@@ -18,4 +18,17 @@ class MyVector < Vector
     self[2] = num
     self
   end
+
+  def self.calculate(vec1, vec2)
+    cos_theta = ((vec1[0] * vec1[1]) + (vec2[0] * vec2[1]))/((Math.sqrt(vec1[1]**2 + vec2[1]**2)) * (Math.sqrt(vec1[0]**2 + vec2[0]**2)))
+
+    theta = Math.acos(cos_theta)
+  end
+
+  def calculate_angle(vectorB)
+    cos_theta = ((self[0] * self[1]) + (vectorB[0] * vectorB[1]))/((Math.sqrt(self[1]**2 + vectorB[1]**2)) * (Math.sqrt(self[0]**2 + vectorB[0]**2)))
+
+    theta = Math.acos(cos_theta)
+    # radians = (theta * Math::PI)/180
+  end
 end
--- test/distance_test.rb
+++ test/distance_test.rb
@@ -6,6 +6,8 @@ class MyVectorTest < MiniTest::Unit::TestCase
   def setup
     @vec1 = MyVector[1, 1]
     @vec2 = MyVector[1, 2]
+    @vec3 = MyVector[3, 3]
+    @vec4 = MyVector[4, 2]
   end
 
   # def test_that_vector_plus_integer_is_valid
@@ -15,4 +17,14 @@ class MyVectorTest < MiniTest::Unit::TestCase
   def test_that_shovel_works_on_vector
     assert_equal Vector[1, 2, 3], @vec2 << 3
   end
+
+  def test_calculates_angle_btw_vectors
+    assert_equal 0.3392926144540446, @vec3.calculate_angle(@vec4)
+  end
+
+  def test_class_method_angle_calculation
+    assert_equal 0.3392926144540446, MyVector.calculate(MyVector[3, 3], MyVector[4, 2])
+  end
+
+
 end
