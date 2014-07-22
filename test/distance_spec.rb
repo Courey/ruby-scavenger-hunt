e3b71c7ae64d2e47b9623eef79b7fa61e736943f
Courey Elliott <coureyelliott@gmail.com>
2014-07-22 13:39:20 -0500

    test

---
+++ test/distance_spec.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+require 'minitest/autorun'
+
+class Whatever < MiniTest::Unit::TestCase
+
+  def setup
+
+  end
+
+  def test_that_vector_plus_integer_is_valid
+    assert_equal 3, thing.add_integer(2)
+  end
+end
